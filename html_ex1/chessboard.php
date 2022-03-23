a040e12c6bd0d6a70340682488f737628baf49f7
Tonton <55071049+antoniosalamanes@users.noreply.github.com>
2022-03-23 11:11:05 +0800

    master: initial commit for chessboard.php

---
+++ html_ex1/chessboard.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chessboard</title>
+</head>
+<body>
+
+<table border=8 bgcolor="#F5DEB3" width=20% height=270px>
+    <tr>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+    </tr>
+    <tr>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+    </tr>
+    <tr>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+    </tr>
+    <tr>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+    </tr>
+    <tr>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+        <td bgcolor="black"></td>
+        <td></td>
+    </tr>
+</table>
+
+</body>
+</html>
