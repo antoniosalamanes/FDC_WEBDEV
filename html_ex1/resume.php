a19553f11f5a03d577fe3828df5bbd1e47562a92
Tonton <55071049+antoniosalamanes@users.noreply.github.com>
2022-03-24 19:17:12 +0800

    master: commit(3)

--- html_ex1/resume.php
+++ html_ex1/resume.php
@@ -52,59 +52,5 @@
         <td> Jun 2069 -- Present </td>
     </tr>
 </table> 
-
-<table border=3>
-    <div> 
-         <tr></tr>
-             <td rowspan=4> <img src="/FDCI_WEBDEV/img/tonton.jfif"> </td>
-             <td> <b> Full Name:</b> Antonio R. Salamanes Jr. </td>
-          <tr>
-            <td> <b>Address:</b> Unit 5A V.Rama Avenue, Brgy. Calamba, Cebu City <td>
-         </tr>
-        
-
-         <tr>
-              <td> <b>Age:</b> 25 <td>
-         </tr>
-
-
-          <tr>
-             <td> <b>Gender:</b> Male <td>
-         </tr>  
-    </div> 
-
-
-    <tr>
-        <td> A </td>
-    </tr>
-
-<!-- 
-    <tr></tr>
-
-
-    <tr></tr>
-
-
-    <tr></tr>
-
-
-    <tr></tr>
-
-
-    <tr></tr>
-
-
-    <tr></tr>
-
-
-    <tr></tr>
-
-
-    <tr></tr> -->
-
-
-</table>
-
-
 </body>
 </html>
